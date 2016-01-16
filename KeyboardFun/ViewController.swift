2ac9ce4a87abc45b04f9041ae22bb4c61e26c7ea
lareinating <lareinating@gmail.com>
2016-01-16 16:31:48 +0800

    first commit

---
+++ KeyboardFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardFun
+//
+//  Created by Lareina Ting on 16/1/16.
+//  Copyright © 2016 Lareina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
